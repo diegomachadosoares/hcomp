04608f82b557476f430990b7db5c42fc74adb915
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-21 18:41:24 -0300

    Adding expressions

---
+++ Add.hs
@@ -0,0 +1,30 @@
+-- Add.hs
+module Add (add) where
+
+import Data.Char
+
+data Expression =
+  Num Int
+  | Add Expression Expression
+  | Sub Expression Expression
+  deriving (Show)
+
+expected x = error $ x ++ " expected"
+
+term x
+  | isDigit x = Num x
+  | otherwise = expected "Digit"
+
+addOperation x
+  | x == '+' = Add
+  | x == '-' = Sub
+  | otherwise = expected "AddOp"
+
+add :: Integer -> Integer -> Integer
+add x y = x + y
+
+sub :: Integer -> Integer ->
+sub x y = x - y
+
+expression (x:[]) = term x
+expression (x:y:zs) = (addOperation y) (expression [x]) (expression zs)
