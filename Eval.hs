a35277eca706d4c3c061f40ff78adada5a48c002
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-22 02:43:38 -0300

    Fixed typo

--- Eval.hs
+++ Eval.hs
@@ -5,7 +5,7 @@ import Syntax
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving(Eq,Ord,Show)
 
-evalExp :: Expr -> Int
+evalExp :: Exp -> Int
 evalExp exp = case exp of
   Add x y -> eval x + eval y
   Sub x y -> eval x - eval y
