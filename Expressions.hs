56b61c08fb86456b31ff0a0ef6eab7d02d08d105
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-24 14:41:58 -0300

    Added evalVar and map struct to M on SMC

--- Expressions.hs
+++ Expressions.hs
@@ -9,27 +9,40 @@ subOp x y = x - y
 mulOp :: Int -> Int -> Int
 mulOp x y = x * y
 
-evalPlus :: ([String],[String],[String]) -> ([String],[String],[String])
+findKey :: String -> [(String,Int)] -> String
+findKey key [] = "nil"
+findKey key ((k,v):xs) = if key == k
+                         then show(v)
+                         else findKey key xs
+
+evalVar :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
+evalVar (a,b,c) = ((findKey (head c) b):a, b, tail c)
+
+evalPlus :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
 evalPlus (a,b,c) = (show(addOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
-evalMinus :: ([String],[String],[String]) -> ([String],[String],[String])
+evalMinus :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
 evalMinus (a,b,c) = (show(subOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
-evalMul :: ([String],[String],[String]) -> ([String],[String],[String])
+evalMul :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
 evalMul (a,b,c) = (show(mulOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
-evalExp :: ([String],[String],[String]) -> ([String],[String],[String])
+-- TODO eval infix expressions
+evalExp :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
 evalExp (a,b,c)
+    -- TODO use pattern match to remove the need for the token ";"
     | x == ";" = (a,b,tail c)
+    -- TODO use any letter as variable!
+    | x == "v" = evalExp (evalVar (a,b,c))
     | x == "+" = evalExp (evalPlus (a,b,c))
     | x == "-" = evalExp (evalMinus (a,b,c))
     | x == "*" = evalExp (evalMul (a,b,c))
