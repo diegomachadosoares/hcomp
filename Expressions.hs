1862bab1d406bb9f3411f470e0c6b5ed3fbab93d
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-25 18:54:00 -0300

    Added Map.Map and removed useless findKey function

--- Expressions.hs
+++ Expressions.hs
@@ -1,5 +1,7 @@
 module Expressions where
 
+import qualified Data.Map as Map
+
 addOp :: Int -> Int -> Int
 addOp x y = x + y
 
@@ -9,35 +11,29 @@ subOp x y = x - y
 mulOp :: Int -> Int -> Int
 mulOp x y = x * y
 
-findKey :: String -> [(String,Int)] -> String
-findKey key [] = "nil"
-findKey key ((k,v):xs) = if key == k
-                         then show(v)
-                         else findKey key xs
+evalVar :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
+evalVar (a,b,c) = ((Map.findWithDefault (head c) "v" b):a, b, tail c)
 
-evalVar :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
-evalVar (a,b,c) = ((findKey (head c) b):a, b, tail c)
-
-evalPlus :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
+evalPlus :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalPlus (a,b,c) = (show(addOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
-evalMinus :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
+evalMinus :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalMinus (a,b,c) = (show(subOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
-evalMul :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
+evalMul :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalMul (a,b,c) = (show(mulOp x y):na,b,tail c)
     where   x = read (a !! 0) :: Int
             y = read (a !! 1) :: Int
             na = drop 2 a
 
 -- TODO eval infix expressions
-evalExp :: ([String],[(String,Int)],[String]) -> ([String],[(String,Int)],[String])
+evalExp :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalExp (a,b,c)
     | null c = (a,b,c)
     -- TODO use any letter as variable!
