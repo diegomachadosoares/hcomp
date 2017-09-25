2175be74b618f9ebef07f2aa790521917eac3a96
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-25 20:38:46 -0300

    Fix return list in some functions

--- BooleanExpressions.hs
+++ BooleanExpressions.hs
@@ -6,13 +6,14 @@ evalT :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String S
 evalT (a,b,c) = (head c:a,b,tail c)
 
 evalEq :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalEq (a,b,c) = if a !! 0 == a !! 1 then (["tt"],b,tail c) else (["ff"],b,tail c)
+evalEq (a,b,c) = if a !! 0 == a !! 1 then ("tt":drop 2 a,b,tail c) else ("ff":drop 2 a,b,tail c)
 
 evalOr :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
 evalOr (a,b,c) = if (a !! 0 == "ff" && a !! 1 == "ff") then ("ff":drop 2 a,b,tail c) else ("tt":drop 2 a,b,tail c)
 
+-- TODO - Fix negation of "tt"
 evalTil :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalTil (a,b,c) = if (head a == "tt") then ("ff":a,b,c) else ("tt":a,b,tail c)
+evalTil (a,b,c) = if (head a == "tt") then ("ff":drop 1 a,b,c) else ("tt":drop 1 a,b,tail c)
 
 evalBoolean :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
 evalBoolean (a,b,c)
