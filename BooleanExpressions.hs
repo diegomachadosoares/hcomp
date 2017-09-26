580c614a77ad0c4cd3fd3fcc259e24749218f8cc
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-26 17:11:09 -0300

    Changing (a,b,c) to (s,m,c)

--- BooleanExpressions.hs
+++ BooleanExpressions.hs
@@ -4,33 +4,27 @@ import qualified Data.Map as Map
 import Expressions
 
 evalT :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalT (a,b,c) = (head c:a,b,tail c)
-
-evalEqHelper (a,_,_) = a
+evalT (s,m,c) = (head c:s,m,tail c)
 
 evalEq :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
---evalEq (a,b,c) = if a !! 0 == a !! 1 then ("tt":drop 2 a,b,tail c) else ("ff":drop 2 a,b,tail c)
-evalEq (a,b,c)
-    | x == y = ("tt":drop 2 a,b,tail c)
-    | otherwise = ("ff":drop 2 a,b,tail c)
-    where x = head evalEqHelper( evalVar(a,b,a!!0:c) )
-          y = head evalEqHelper( evalVar(a,b,a!!1:c) )
+evalEq (s,m,c) = if s !! 0 == s !! 1 then ("tt":drop 2 s,m,tail c) else ("ff":drop 2 s,m,tail c)
 
 evalOr :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalOr (a,b,c) = if (a !! 0 == "ff" && a !! 1 == "ff") then ("ff":drop 2 a,b,tail c) else ("tt":drop 2 a,b,tail c)
+evalOr (s,m,c) = if (s !! 0 == "ff" && s !! 1 == "ff") then ("ff":drop 2 s,m,tail c) else ("tt":drop 2 s,m,tail c)
 
 -- TODO - Fix negation of "tt"
-evalTil :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalTil (a,b,c) = if (head a == "tt") then ("ff":drop 1 a,b,c) else ("tt":drop 1 a,b,tail c)
+evalNot :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
+evalNot (s,m,c)
+    | head s == "tt" = ("ff":drop 1 s,m,tail c)
+    | head s == "ff" = ("tt":drop 1 s,m,tail c)
 
 evalBoolean :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String, [String])
-evalBoolean (a,b,c)
-    | null c = (a,b,c)
-    | x == "tt" = evalBoolean (evalT (a,b,c))
-    | x == "ff" = evalBoolean (evalT (a,b,c))
-    | x == "=" = evalBoolean (evalEq (a,b,c))
-    | x == "or" = evalBoolean (evalOr (a,b,c))
-    | x == "~" = evalBoolean (evalTil(a,b,c))
-    -- TODO improve edge case!
-    | otherwise = evalBoolean (head c:a,b,tail c)
+evalBoolean (s,m,c)
+    | null c = (s,m,c)
+    | x == "tt" = evalBoolean (evalT (s,m,c))
+    | x == "ff" = evalBoolean (evalT (s,m,c))
+    | x == "=" = evalBoolean (evalEq (s,m,c))
+    | x == "or" = evalBoolean (evalOr (s,m,c))
+    | x == "~" = evalBoolean (evalNot (s,m,c))
+    | x `elem` vars = evalBoolean (evalExp (s,m,c))
     where x = head c
