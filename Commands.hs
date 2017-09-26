1f212496d5ba371b497804d8a6414b660a072b88
Diego M. Soares <diegomachadosoares@gmail.com>
2017-09-26 19:07:44 -0300

    Fixed while

--- Commands.hs
+++ Commands.hs
@@ -1,35 +1,36 @@
 module Commands where
 
 import qualified Data.Map as Map
+import Data.List
+import Expressions
+import BooleanExpressions
 
 evalNil :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalNil (s,m,c) = (s,m,tail c)
 
 evalAttr :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
-evalAttr (s,m,c) = (drop 2 s,Map.insert (s !! 1) (s !! 0) m,tail c)
+evalAttr (s,m,c) = (tail s,Map.insert (head c) (head s) m,tail c)
+
+first (s,_,_) = s
 
 evalIF :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalIF (s,m,c)
     | x == "tt" = (s,m,tail (takeWhile (/="else") (tail c)))
     | x == "ff" = (s,m,tail (dropWhile (/="else") c))
-    where x = head c
-
-whileHelper (s,_,_) = a
-
+    where x = head (first (evalBoolean(s,m,takeWhile (/="then") c)))
 
 evalWhile :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalWhile (s,m,c)
-    | x == "while" = evalCMD((c!!1):(c!!3):s,m,(c!!1):"while":c)
-    | x == "ff" = (s,m,c)
-    where x = head c
-
+    | x == "tt" = evalCMD(s,m,concat [tail ( (dropWhile (/="do") (takeWhile(/="fimDo") c) ) ),["while"],c])
+    | x == "ff" = (s,m,tail (dropWhile (/="fimDo") c))
+    where x = head (first (evalBoolean(s,m,takeWhile (/="do") c)))
 
 evalCMD :: ([String],Map.Map String String,[String]) -> ([String],Map.Map String String,[String])
 evalCMD (s,m,c)
     | null c = (s,m,c)
     | x == "nil" = evalCMD (evalNil (s,m,c))
-    | x == ":=" = evalCMD (evalAttr (s,m,c))
+    | x == ":=" = evalCMD (evalAttr (s,m,tail c))
     | x == "if" = evalCMD (evalIF (s,m,tail c))
-    | x == "while" = evalCMD (evalWhile (s,m,c))
-    | otherwise = evalCMD (head c:s,m,tail c)
+    | x == "while" = evalCMD (evalWhile (s,m,tail c))
+    | otherwise = evalCMD (evalExp (s,m,c))
     where x = head c
