a7d9003e5604c0c130059da39b9d840591ec2da7
Diego M. Soares <diegomachadosoares@gmail.com>
2017-10-30 13:26:25 -0200

    WIP

--- Declaration.hs
+++ Declaration.hs
@@ -2,18 +2,21 @@ module Declaration where
 
 import qualified Data.Map as Map
 import Data.List
+
+import Syntax
 import Expressions
 import BooleanExpressions
+import ESMC
 
 -- TODO define kind x
 
-evalDec :: (Map.Map Int String, [String], Map.Map String String, [String]) -> (Map.Map Int String, [String], Map.Map String String, [String])
+evalDec :: (E, S, M, C) -> (E, S, M, C)
 evalDec (e,s,m,c)
    | x == "const" = evalConst (e,s,m,tail c)
    | x == "var" = evalEVar (e,s,m,tail c)
    where x = head c
    
-evalConst :: (Map.Map Int String, [String], Map.Map String String, [String]) -> (Map.Map Int String, [String], Map.Map String String, [String])
+evalConst :: (E, S, M, C) -> (E, S, M, C)
 evalConst (e,s,m,c)
    | x == "int" = evalConst (evalDecExp(e,s,m,tail c))
    | x == "boolean" = evalConst (evalDecBoolean(e,s,m,tail c))
@@ -23,7 +26,7 @@ evalConst (e,s,m,c)
          f = Map.insert (head c) (head s) f
 
 -- TODO get position of c in m to z
-evalEVar :: (Map.Map Int String, [String], Map.Map String String, [String]) -> (Map.Map Int String, [String], Map.Map String String, [String])
+evalEVar :: (E, S, M, C) -> (E, S, M, C)
 evalEVar (e,s,m,c)
    | x == "int" = evalConst (evalDecExp(e,s,m,tail c))
    | x == "boolean" = evalConst (evalDecBoolean(e,s,m,tail c))
@@ -37,7 +40,7 @@ first (_,s,_,_) = s
 evalDecExp :: (E, S, M, C) -> (E, S, M, C)
 evalDecExp (e,s,m,c)
     | null c = (e,s,m,c)
-	| x == "if" = evalExpIF (e,s,m,tail c)
+    | x == "if" = evalExpIF (e,s,m,tail c)
     | x `elem` vars = evalExp (evalVar (e,s,m,c))
     | x == "+" = evalExp (evalPlus (e,s,m,c))
     | x == "-" = evalExp (evalMinus (e,s,m,c))
@@ -50,13 +53,13 @@ evalExpIF :: (E, S, M, C) -> (E, S, M, C)
 evalExpIF (e,s,m,c)
     | x == "tt" = (e,v,m,":="(tail (dropWhile (/="fimElse") c)))
     | x == "ff" = evalDecExp(e,s,m,tail (dropWhile (/="else") c))
-	where x = head (first (evalBoolean(e,s,m,takeWhile (/="then") c)))
+    where x = head (first (evalBoolean(e,s,m,takeWhile (/="then") c)))
           v = first evalDecExp(e,s,m,takeWhile(/="else"))
 		  
 evalDecBoolean :: (E, S, M, C) -> (E, S, M, C)
 evalDecBoolean (e,s,m,c)
     | null c = (e,s,m,c)
-	| x == "if" = evalBoolIF (e,s,m,tail c)
+    | x == "if" = evalBoolIF (e,s,m,tail c)
     | x == "tt" = evalBoolean (evalT (e,s,m,c))
     | x == "ff" = evalBoolean (evalT (e,s,m,c))
     | x == "=" = evalBoolean (evalEq (e,s,m,c))
@@ -71,6 +74,6 @@ evalBoolIF :: (E, S, M, C) -> (E, S, M, C)
 evalBoolIF (e,s,m,c)
     | x == "tt" = (e,v,m,":="(tail (dropWhile (/="fimElse") c)))
     | x == "ff" = evalDecBoolean(e,s,m,tail (dropWhile (/="else") c))
-	where x = head (first (evalBoolean(e,s,m,takeWhile (/="then") c)))
-          v = first evalDecBoolean(e,s,m,takeWhile(/="else"))		  
+    where x = head (first (evalBoolean(e,s,m,takeWhile (/="then") c)))
+          v = first evalDecBoolean(e,s,m,takeWhile(/="else"))
 	
