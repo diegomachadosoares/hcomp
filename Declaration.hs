0a660a8602d89c79b1f1b891bfb8e85d6f03a0af
Diego M. Soares <diegomachadosoares@gmail.com>
2017-11-06 18:54:37 -0200

    Fixing declaration of command (IF)

--- Declaration.hs
+++ Declaration.hs
@@ -1,6 +1,7 @@
 module Declaration where
 
 import qualified Data.Map as Map
+import qualified Data.Vector as Vector
 import Data.List
 import Data.Char
 
@@ -12,34 +13,29 @@ evalDec :: (E, S, M, C) -> (E, S, M, C)
 evalDec (e,s,m,c)
    | x == "const" = evalConst (e,s,m,tail c)
    | x == "var" = evalEVar (e,s,m,tail c)
+   | otherwise = (e,s,m,c)
    where x = head c
    
 evalConst :: (E, S, M, C) -> (E, S, M, C)
 evalConst (e,s,m,c)
    | x == "int" = evalConst (evalDecExp(e,s,m,tail c))
-   | x == "boolean" = evalConst (evalDecBoolean(e,s,m,tail c))
-   | x == ":=" = (f,tail s,m,tail c)
-   | x == "fimElse" = (f,tail s,m,tail c)
+   | x == "boolean" = evalConst (evalDecExp(e,s,m,tail c))
+   | x == ":=" = (Map.insert (head (tail c)) ("c",head s) e,tail s,m,tail (tail c))
+   | x == "fimElse" = (Map.insert (head (tail c)) ("c",head s) e,tail s,m,tail (tail c))
    where x = head c
-         f = Map.insert (head c) ("c",head s) e
 
 evalEVar :: (E, S, M, C) -> (E, S, M, C)
 evalEVar (e,s,m,c)
-   | x == "int" = evalConst (evalDecExp(e,s,m,tail c))
-   | x == "boolean" = evalConst (evalDecBoolean(e,s,m,tail c))
-   | x == ":=" = (f,tail s,Map.insert (head c) (head s) m,tail c)
-   | x == "fimElse" = (f,tail s,Map.insert (head c) (head s) m,tail c)
+   | x == "int" = evalEVar (evalDecExp(e,s,m,tail c))
+   | x == "boolean" = evalEVar (evalDecExp(e,s,m,tail c))
+   | x == ":=" = (Map.insert (head (tail c)) ("v", show ((Vector.length m))) e,tail s,m Vector.++ (Vector.singleton (head s)),tail (tail c))
+   | x == "fimElse" = (Map.insert (head (tail c)) ("v", show ((Vector.length m))) e,tail s,m Vector.++ (Vector.singleton (head s)),tail (tail c))
    where x = head c
-         f = Map.insert (head c) ("v",show (Map.findIndex (head c) m)) e
 
 evalDecExp :: (E, S, M, C) -> (E, S, M, C)
 evalDecExp (e,s,m,c)
     | null c = (e,s,m,c)
     | x == "if" = evalExpIF (e,s,m,tail c)
-    | x `elem` vars = evalExp (evalVar (e,s,m,c))
-    | x == "+" = evalExp (evalPlus (e,s,m,c))
-    | x == "-" = evalExp (evalMinus (e,s,m,c))
-    | x == "*" = evalExp (evalMul (e,s,m,c))
     | isDigit (head x) = evalExp (e,x:s,m,tail c)
     | otherwise = (e,s,m,c)
     where x = head c
@@ -49,25 +45,4 @@ evalExpIF (e,s,m,c)
     | x == "tt" = (e,v,m,":=":(tail (dropWhile (/="fimElse") c)))
     | x == "ff" = evalDecExp(e,s,m,tail (dropWhile (/="else") c))
     where x = head (filterS (evalExp(e,s,m,takeWhile (/="then") c)))
-          v = filterS (evalDecExp(e,s,m,takeWhile(/="else") c))
-
-evalDecBoolean :: (E, S, M, C) -> (E, S, M, C)
-evalDecBoolean (e,s,m,c)
-    | null c = (e,s,m,c)
-    | x == "if" = evalBoolIF (e,s,m,tail c)
-    | x == "tt" = evalExp (evalT (e,s,m,c))
-    | x == "ff" = evalExp (evalT (e,s,m,c))
-    | x == "=" = evalExp (evalEq (e,s,m,c))
-    | x == "or" = evalExp (evalOr (e,s,m,c))
-    | x == "~" = evalExp (evalNot (e,s,m,c))
-    | x `elem` vars = evalExp (evalExp (e,s,m,c))
-    | isDigit (head x) = evalExp (evalExp (e,s,m,c))
-    | otherwise = (e,s,m,c)
-    where x = head c
-
-evalBoolIF :: (E, S, M, C) -> (E, S, M, C)
-evalBoolIF (e,s,m,c)
-    | x == "tt" = (e,v,m,":=":(tail (dropWhile (/="fimElse") c)))
-    | x == "ff" = evalDecBoolean(e,s,m,tail (dropWhile (/="else") c))
-    where x = head (filterS (evalExp(e,s,m,takeWhile (/="then") c)))
-          v = filterS (evalDecBoolean(e,s,m,takeWhile(/="else") c))
+          v = filterS (evalExp(e,s,m,(tail (dropWhile (/="then") (takeWhile(/="else") c)))))
