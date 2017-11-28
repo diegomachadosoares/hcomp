377c88c69dcf3a043e5b13bd3f5a525f3b059b05
Diego M. Soares <diegomachadosoares@gmail.com>
2017-11-28 16:56:14 -0200

    Merging Declaration and Commands

--- Commands.hs
+++ Commands.hs
@@ -8,7 +8,6 @@ import Data.Char
 import Syntax
 import HelperTools
 import Expressions
-import Declaration
 
 evalCMD :: (E, S, M, C, O) -> (E, S, M, C, O)
 
@@ -36,3 +35,24 @@ evalCMD (e,s,m,(Ccom (Const a b exp)):c,o) = evalCMD (evalDec(e,s,m,(Ccom (Const
 evalCMD (e,s,m,(Ccom (Sequence a b)):c,o) = evalCMD (e,s,m,(Ccom a):(Ccom b):c,o)
 evalCMD (e,s,m,(Ccom (Print exp)):c,o) = evalCMD (e,s,m,c,head(filterS (evalExp(e,s,m,[(Cexp exp)],o))):o)
 evalCMD (e,s,m,c,o) = (e,s,m,c,o)
+
+evalDecExp :: (E, S, M, C, O) -> (E, S, M, C, O)
+evalDecExp (e,s,m,[],o) = (e,s,m,[],o)
+evalDecExp (e,s,m,(Cexp (IfExp a b d)):c,o)= evalExpIF (e,s,m,(Cexp (IfExp a b d)):c,o)
+evalDecExp (e,s,m,c,o)= evalExp(e,s,m,c,o)
+
+evalExpIF :: (E, S, M, C, O) -> (E, S, M, C, O)
+evalExpIF (e,s,m,(Cexp (IfExp a b d)):c,o)
+    | x = (e,v,m,tail c,o)
+    | otherwise = evalDecExp(e,s,m,tail c,o)
+    where x = rBVal (head (filterS (evalExp(e,s,m,[Cexp a],o))))
+          v = filterS (evalExp(e,s,m,[Cexp b],o))
+
+evalDec :: (E, S, M, C, O) -> (E, S, M, C, O)
+evalDec (e,s,m,[],o) = (e,s,m,[],o)
+evalDec (e,s,m,(Ccom (Var a b d)):c,o) = free (evalCMD (((Map.insert a (BndLoc (Loc ( V.length m)))) e),s,m V.++ (V.singleton (convValStr(head(filterS (evalDecExp(e,s,m,[Cexp d],o)))))),c,o))
+evalDec (e,s,m,(Ccom (Const a b d)):c,o) = (Map.insert a (convValBnd(head (filterS (evalDecExp(e,s,m,[Cexp d],o))))) e,s,m,c,o)
+evalDec (e,s,m,(Ccom (ProcR id f bl)):c,o) = (Map.insert id (BndAbs (f,(Ccom (ProcR id f bl)):bl)) e,s,m,c,o)
+evalDec (e,s,m,c,o) = (e,s,m,c,o)
+
+free (e,s,m,c,o) = (e,s,m,c,o)
